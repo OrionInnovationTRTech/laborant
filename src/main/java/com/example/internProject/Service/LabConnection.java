9dbeea3160421d68557c334b095230eeb3b49ab6
EgeAtaCeylan <eata@sabanciuniv.edu>
2022-07-06 14:41:16 +0300

    ADDED LAB CONNECTION CLASS
    Added lab connection class with function connect and execute command to connect to to lab with the given host username, password and given command to execute to command after making the connection to the lab over SSH then returning the response of the lab back.

---
+++ src/main/java/com/example/internProject/Service/LabConnection.java
@@ -0,0 +1,53 @@
+package com.example.internProject.Service;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+
+public class LabConnection
+{
+    
+
+    public static void connectAndExecuteCommand(String username, String password,
+                                           String host, int port, String command) throws Exception {
+
+        Session session = null;
+        ChannelExec channel = null;
+
+        try {
+            session = new JSch().getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+            channel.setOutputStream(responseStream);
+            channel.connect();
+
+            while (channel.isConnected()) {
+                Thread.sleep(100);
+            }
+
+            String responseString = new String(responseStream.toByteArray());
+            System.out.println(responseString);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if (session != null) {
+                session.disconnect();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+        }
+
+    }
+
+}
