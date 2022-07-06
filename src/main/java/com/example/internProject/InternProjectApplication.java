8c95747b408417f785c7c7332aed46fadcf9f30e
EgeAtaCeylan <eata@sabanciuniv.edu>
2022-07-06 12:44:53 +0300

    INITIAL PROJECT

---
+++ src/main/java/com/example/internProject/InternProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.internProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternProjectApplication {
+
+	public static void main(String[] args) throws Exception
+	{
+		SpringApplication.run(InternProjectApplication.class, args);
+	}
+
+}
