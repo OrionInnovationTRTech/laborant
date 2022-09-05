910da0a09722a47fec509cc27114b362fd349e8c
EgeAtaCeylan <eata@sabanciuniv.edu>
2022-09-05 13:16:26 +0300

    ADDED GET ALL LAB VERSIONS FUNCTION TO EXTRACT THE LAB VERSIONS FOR EACH LAB IN THE DATABASE

--- src/main/java/com/example/internProject/Service/LabService.java
+++ src/main/java/com/example/internProject/Service/LabService.java
@@ -135,4 +135,42 @@ public class LabService
 
     }
 
+    public List<String> getAllLabVersions(){
+        List<String> labVersions = new ArrayList<>();
+        List<Lab> allLabs = adminService.getAllLabs();
+        if (allLabs.size() == 0)
+        {
+            labVersions.add("There aren't any labs in the database");
+            return labVersions;
+        }
+        else {
+            String outputString = "";
+            for (Lab currentLab : allLabs
+            ) {
+                outputString = "";
+                outputString += currentLab.getLabName() + " \n";
+                try {
+                    outputString +=
+                            connectAndExecuteCommand(currentLab.getUserName(),
+                                    currentLab.getPassword(), currentLab.getHost(), currentLab.getPort(),
+                                    "sudo wae-status");
+                    outputString += "\n  \n";
+                    List<String> tokens = new ArrayList<>();
+                    StringTokenizer tokenizer = new StringTokenizer(outputString);
+                    while (tokenizer.hasMoreElements())
+                        tokens.add(tokenizer.nextToken());
+                    String currentVersion = tokens.get(15);
+                    labVersions.add(currentVersion);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    labVersions.add("UNABLE TO CONNECT");
+
+
+                }
+
+            }
+            return labVersions;
+        }
+    }
+
 }
